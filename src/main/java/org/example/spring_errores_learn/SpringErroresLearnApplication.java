300ac09382cd51838137cd6ebd56c49ffed38a17
itzcoatl397 <itzcoatl3b@gmail.com>
2025-01-14 19:20:28 -0600

    d

---
+++ src/main/java/org/example/spring_errores_learn/SpringErroresLearnApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_errores_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErroresLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringErroresLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_errores_learn/SpringErroresLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_errores_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErroresLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
